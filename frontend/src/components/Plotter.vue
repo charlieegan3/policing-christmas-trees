6901d604e7547d6ab6f946ec154e8a1d7f070216
Charlie Egan <charlieegan3@users.noreply.github.com>
2019-11-12 21:54:59 +0000

    Add topper plotting code

--- frontend/src/components/Plotter.vue
+++ frontend/src/components/Plotter.vue
@@ -18,16 +18,24 @@ export default {
   methods: {
     draw: function(ctx) {
       ctx.font = "30px Arial";
-      ctx.fillText(String.fromCodePoint(11088), 10, 50);
-      ctx.fillText(String.fromCodePoint(128519), 30, 50);
+      var outline = this.transformOutline(this.tree.outline);
 
-		var outline = this.transformOutline(this.tree.outline);
-		outline.push(outline[0]);
-		for (var i = 0; i < outline.length - 1; i++) {
-		  ctx.moveTo(outline[i][0],outline[i][1]);
-		  ctx.lineTo(outline[i+1][0],outline[i+1][1]);
-		  ctx.stroke();
-		}
+      var treeTop = outline[Math.floor(outline.length/2)];
+
+      var topperString = String.fromCodePoint(128519);
+      if (this.tree.topper == "star") {
+        topperString = String.fromCodePoint(11088);
+      }
+
+      ctx.fillText(topperString, treeTop[0]-15, treeTop[1]-5);
+
+      // add a point to complete the shape
+      outline.push(outline[0]);
+      for (var i = 0; i < outline.length - 1; i++) {
+        ctx.moveTo(outline[i][0],outline[i][1]);
+        ctx.lineTo(outline[i+1][0],outline[i+1][1]);
+        ctx.stroke();
+      }
 
     },
 	transformOutline: function(outline) {
