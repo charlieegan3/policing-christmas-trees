7faafa5c396b3860f3dfaa7dfbc636cf1f7524f6
Charlie Egan <charlieegan3@users.noreply.github.com>
2019-11-23 21:49:23 +0000

    Update readme content to match about

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Policing Christmas Trees
 
-A wise man once said: "Organised, safe fun is the best kind - _especially_ when
-Christmas trees are involved."
+A wise man once said: "Safe, organised fun is the best kind of fun -
+_especially_ when Christmas trees are involved."
+
+This project was prepared as a demo of [OPA](https://www.openpolicyagent.org)
+for the 2019 _Christmas_ Show & Tell of the
+[London Computation Club](https://london.computation.club).
+
+While this is a really dumb idea for a project; I've found it helpful to apply a
+'work thing' in a completely different (if fictional) domain.
+
+You can view a demo of the project at
+[xmas-trees.charlieegan3.com](https://xmas-trees.charlieegan3.com).
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,6 +3,6 @@
 		<h2 class="f4 f3-ns" id="about">About</h2>
 		<p>This project was prepared as a demo of <a href="https://www.openpolicyagent.org">OPA</a> for the 2019 <em>Christmas</em> Show &amp; Tell of the <a href="https://london.computation.club">London Computation Club</a>.</p>
 		<p>You can find the project on <a href="https://github.com/charlieegan3/policing-christmas-trees">GitHub here</a>.</p>
-		<p>While this is a really dumb idea for a project; I've found it helpful to apply a 'work thing' in a complete;y different (if fictional) domain.</p>
+		<p>While this is a really dumb idea for a project; I've found it helpful to apply a 'work thing' in a completely different (if fictional) domain.</p>
 	</div>
 </template>
