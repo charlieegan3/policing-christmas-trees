d13754c3722bdddeb6582933235ddcc657e15a68
Charlie Egan <charlieegan3@users.noreply.github.com>
2019-11-23 17:07:11 +0000

    Format code

--- frontend/src/components/Plotter.vue
+++ frontend/src/components/Plotter.vue
@@ -1,133 +1,137 @@
 <template>
-  <canvas :width="width" :height="height" :data-trigger="ctx &amp;&amp; redraw"/>
+	<canvas :width="width" :height="height" :data-trigger="ctx &amp;&amp; redraw"/>
 </template>
 
 <script>
-
 export default {
-  props: ["tree", "cursor", "width", "height", "scaleFactor"],
-  data() {
-	  return { ctx: null };
-  },
-  mounted() {
-	var canvas = this.$el;
-	var component = this;
-    this.$el.addEventListener('mousemove', function(evt) {
-      var rect = canvas.getBoundingClientRect();
-      var cursorPos = { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
-	  component.$emit("mousemove", cursorPos);
-    }, false);
-    this.$el.addEventListener('click', function(evt) {
-      var rect = canvas.getBoundingClientRect();
-      var cursorPos = { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
-	  component.$emit("clicked", cursorPos);
-    }, false);
-	this.ctx = canvas.getContext('2d');
-  },
-  methods: {
-    draw: function(ctx) {
-      // draw cursor
-      ctx.font = "10px Arial";
-      ctx.fillStyle = "#ccc";
-      ctx.fillText(String.fromCodePoint(10060), this.cursor.x-5, this.cursor.y+5);
-
-		for (var i = 0; i < this.width; i+=this.scaleFactor) {
-			for (var j = 0; j < this.height; j+=this.scaleFactor) {
-      			ctx.fillText("x", i, j);
-			}
-		}
-
-	  // don't render if missing outline
-		if (this.tree.outline.length == 0) {
-			return
-		}
-
-      ctx.font = "30px Arial";
-      var outline = this.transformPoints(this.tree.outline);
-
-      var treeTop = outline[Math.floor(outline.length/2)];
-
-      var topperString = String.fromCodePoint(128519);
-      if (this.tree.topper == "star") {
-        topperString = String.fromCodePoint(11088);
-      }
-      if (this.tree.topper == "poop") {
-        topperString = String.fromCodePoint(128169);
-      }
-
-      ctx.fillText(topperString, treeTop[0]-15, treeTop[1]-5);
-
-      // add a point to complete the shape
-      ctx.lineWidth = 2;
-      ctx.strokeStyle = "green";
-      ctx.beginPath();
-      outline.push(outline[0]);
-      for (var i = 0; i < outline.length - 1; i++) {
-        ctx.moveTo(outline[i][0],outline[i][1]);
-        ctx.lineTo(outline[i+1][0],outline[i+1][1]);
-        ctx.stroke();
-      }
-
-      var tinsels = this.transformTinsels(this.tree.tinsels);
-      ctx.beginPath();
-      ctx.lineWidth = 10;
-      ctx.strokeStyle = "red";
-      for (var i = 0; i < tinsels.length; i++) {
-		ctx.moveTo(tinsels[i][0][0],tinsels[i][0][1]);
-		ctx.lineTo(tinsels[i][1][0],tinsels[i][1][1]);
-		ctx.stroke();
-      }
-
-		var baubles = this.transformPoints(this.tree.baubles);
-        var bauble = String.fromCodePoint(128302);
-		for (var i = 0; i < baubles.length; i++) {
-			ctx.save();
-			ctx.translate(baubles[i][0]+15, baubles[i][1]+5);
-			ctx.rotate(Math.PI);
-
-			ctx.fillText(bauble, 0, 0);
-			ctx.restore();
-		}
-    },
-	transformPoints: function(outline) {
-		var transformedOutline = [];
-		for (var i = 0; i < outline.length; i++) {
-			transformedOutline.push(this.transformPoint(outline[i]))
-		}
-		return transformedOutline
+	props: ["tree", "cursor", "width", "height", "scaleFactor"],
+	data() {
+		return {
+			ctx: null
+		};
 	},
-	transformTinsels: function(tinsels) {
-		var transformedTinsels = [];
-		for (var i = 0; i < tinsels.length; i++) {
-			if (tinsels[i].length == 2) {
-				transformedTinsels.push([
-					this.transformPoint(tinsels[i][0]),
-					this.transformPoint(tinsels[i][1]),
-				]);
-			}
-		}
-		return transformedTinsels
+	mounted() {
+		var canvas = this.$el;
+		var component = this;
+		this.$el.addEventListener('mousemove', function(evt) {
+			var rect = canvas.getBoundingClientRect();
+			var cursorPos = {
+				x: evt.clientX - rect.left,
+				y: evt.clientY - rect.top
+			};
+			component.$emit("mousemove", cursorPos);
+		}, false);
+		this.$el.addEventListener('click', function(evt) {
+			var rect = canvas.getBoundingClientRect();
+			var cursorPos = {
+				x: evt.clientX - rect.left,
+				y: evt.clientY - rect.top
+			};
+			component.$emit("clicked", cursorPos);
+		}, false);
+		this.ctx = canvas.getContext('2d');
 	},
-	transformPoint: function(point) {
-		return [
-			point[0] * this.scaleFactor,
-			this.height - point[1] * this.scaleFactor,
-		]
-	}
-  },
-  computed: {
-    redraw() {
-      let canvas = this.$el;
-      canvas.width = this.width;
-      canvas.height = this.height;
-      this.ctx.clearRect(0, 0, this.width, this.height);
-      this.ctx.save();
-      this.draw(this.ctx);
-      this.ctx.restore();
-    },
-  },
+	methods: {
+		draw: function(ctx) {
+			// draw cursor
+			ctx.font = "10px Arial";
+			ctx.fillStyle = "#ccc";
+			ctx.fillText(String.fromCodePoint(10060), this.cursor.x - 5, this.cursor.y + 5);
+
+			for (var i = 0; i < this.width; i += this.scaleFactor) {
+				for (var j = 0; j < this.height; j += this.scaleFactor) {
+					ctx.fillText("x", i, j);
+				}
+			}
+
+			// don't render if missing outline
+			if (this.tree.outline.length == 0) {
+				return
+			}
+
+			ctx.font = "30px Arial";
+			var outline = this.transformPoints(this.tree.outline);
+
+			var treeTop = outline[Math.floor(outline.length / 2)];
+
+			var topperString = String.fromCodePoint(128519);
+			if (this.tree.topper == "star") {
+				topperString = String.fromCodePoint(11088);
+			}
+			if (this.tree.topper == "poop") {
+				topperString = String.fromCodePoint(128169);
+			}
+
+			ctx.fillText(topperString, treeTop[0] - 15, treeTop[1] - 5);
+
+			// add a point to complete the shape
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = "green";
+			ctx.beginPath();
+			outline.push(outline[0]);
+			for (var i = 0; i < outline.length - 1; i++) {
+				ctx.moveTo(outline[i][0], outline[i][1]);
+				ctx.lineTo(outline[i + 1][0], outline[i + 1][1]);
+				ctx.stroke();
+			}
+
+			var tinsels = this.transformTinsels(this.tree.tinsels);
+			ctx.beginPath();
+			ctx.lineWidth = 10;
+			ctx.strokeStyle = "red";
+			for (var i = 0; i < tinsels.length; i++) {
+				ctx.moveTo(tinsels[i][0][0], tinsels[i][0][1]);
+				ctx.lineTo(tinsels[i][1][0], tinsels[i][1][1]);
+				ctx.stroke();
+			}
+
+			var baubles = this.transformPoints(this.tree.baubles);
+			var bauble = String.fromCodePoint(128302);
+			for (var i = 0; i < baubles.length; i++) {
+				ctx.save();
+				ctx.translate(baubles[i][0] + 15, baubles[i][1] + 5);
+				ctx.rotate(Math.PI);
+
+				ctx.fillText(bauble, 0, 0);
+				ctx.restore();
+			}
+		},
+		transformPoints: function(outline) {
+			var transformedOutline = [];
+			for (var i = 0; i < outline.length; i++) {
+				transformedOutline.push(this.transformPoint(outline[i]))
+			}
+			return transformedOutline
+		},
+		transformTinsels: function(tinsels) {
+			var transformedTinsels = [];
+			for (var i = 0; i < tinsels.length; i++) {
+				if (tinsels[i].length == 2) {
+					transformedTinsels.push([
+						this.transformPoint(tinsels[i][0]),
+						this.transformPoint(tinsels[i][1]),
+					]);
+				}
+			}
+			return transformedTinsels
+		},
+		transformPoint: function(point) {
+			return [
+				point[0] * this.scaleFactor,
+				this.height - point[1] * this.scaleFactor,
+			]
+		}
+	},
+	computed: {
+		redraw() {
+			let canvas = this.$el;
+			canvas.width = this.width;
+			canvas.height = this.height;
+			this.ctx.clearRect(0, 0, this.width, this.height);
+			this.ctx.save();
+			this.draw(this.ctx);
+			this.ctx.restore();
+		},
+	},
 }
 </script>
-
-<style>
-</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,161 +1,191 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <button v-on:click="tree.topper = 'star'">&#11088;</button>
-    <button v-on:click="tree.topper = 'angel'">&#128519;</button>
-    <button v-on:click="tree.topper = 'poop'">&#128169;</button>
-    <button v-on:click="tree.outline = []; tree.tinsels = []">Clear</button>
-    <br/>
-    <button v-on:click="validate">Validate</button>
-	<p v-if="validation.valid">Your tree is allowed</p>
-	<p v-if="validation.valid == false">Your tree is bad</p>
-	<p>{{ validation.status }}</p>
-	<ul>
-	  <li v-for="message in validation.messages">
-		{{ message }}
-	  </li>
-	</ul>
-    <br/>
-	<p>PlotMode: {{ plottingMode }}</p>
-	<button v-on:click="plottingMode = 'tree'">tree</button>
-	<button v-on:click="plottingMode = 'tinsel'">tinsel</button>
-	<button v-on:click="plottingMode = 'bauble'">bauble</button>
-    <br/>
 	<div>
-		<div class="dib fl">
-			<Plotter class="ba"
-					 :width="height"
-					 :height="height"
-					 :scaleFactor="gridSize"
-					 :tree="tree"
-					 :cursor="cursor"
-					 @mousemove="updateCursor"
-					 @clicked="addPoint"/>
-		</div>
-		<div class="f7 fl">
-			<textarea rows="50">{{ JSON.stringify(this.tree, null, 2) }}</textarea>
-		</div>
-		<div class="f7 fl">
-			<p>Outline</p>
-			<ul>
-				<li v-for="point, index in tree.outline">
-					{{ point }}
-    				<button v-on:click="tree.outline.splice(index, 1)">X</button>
-				</li>
-			</ul>
-		</div>
-		<div class="f7 fl">
-			<p>Tinsels</p>
-			<ul>
-				<li v-for="tinsel, index in tree.tinsels">
-					{{ tinsel }}
-    				<button v-on:click="tree.tinsels.splice(index, 1)">X</button>
-				</li>
-			</ul>
-		</div>
-		<div class="f7 fl">
-			<p>Baubles</p>
-			<ul>
-				<li v-for="bauble, index in tree.baubles">
-					{{ bauble }}
-    				<button v-on:click="tree.baubles.splice(index, 1)">X</button>
-				</li>
-			</ul>
+		<h1>Home</h1>
+		<button v-on:click="tree.topper = 'star'">&#11088;</button>
+		<button v-on:click="tree.topper = 'angel'">&#128519;</button>
+		<button v-on:click="tree.topper = 'poop'">&#128169;</button>
+		<button v-on:click="tree.outline = []; tree.tinsels = []">Clear</button>
+		<br/>
+		<button v-on:click="validate">Validate</button>
+		<p v-if="validation.valid">Your tree is allowed</p>
+		<p v-if="validation.valid == false">Your tree is bad</p>
+		<p>{{ validation.status }}</p>
+		<ul>
+			<li v-for="message in validation.messages">
+				{{ message }}
+			</li>
+		</ul>
+		<br/>
+		<p>PlotMode: {{ plottingMode }}</p>
+		<button v-on:click="plottingMode = 'tree'">tree</button>
+		<button v-on:click="plottingMode = 'tinsel'">tinsel</button>
+		<button v-on:click="plottingMode = 'bauble'">bauble</button>
+		<br/>
+		<div>
+			<div class="dib fl">
+				<Plotter class="ba"
+					:width="height"
+					:height="height"
+					:scaleFactor="gridSize"
+					:tree="tree"
+					:cursor="cursor"
+					@mousemove="updateCursor"
+					@clicked="addPoint"/>
+			</div>
+			<div class="f7 fl">
+				<textarea rows="50">{{ JSON.stringify(this.tree, null, 2) }}</textarea>
+			</div>
+			<div class="f7 fl">
+				<p>Outline</p>
+				<ul>
+					<li v-for="point, index in tree.outline">
+						{{ point }}
+						<button v-on:click="tree.outline.splice(index, 1)">X</button>
+					</li>
+				</ul>
+			</div>
+			<div class="f7 fl">
+				<p>Tinsels</p>
+				<ul>
+					<li v-for="tinsel, index in tree.tinsels">
+						{{ tinsel }}
+						<button v-on:click="tree.tinsels.splice(index, 1)">X</button>
+					</li>
+				</ul>
+			</div>
+			<div class="f7 fl">
+				<p>Baubles</p>
+				<ul>
+					<li v-for="bauble, index in tree.baubles">
+						{{ bauble }}
+						<button v-on:click="tree.baubles.splice(index, 1)">X</button>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
-
-  </div>
 </template>
+
 
 <script>
 import Plotter from '@/components/Plotter.vue'
 import axios from 'axios';
 
 export default {
-  name: 'home',
-  data() {
-    return {
-		width: 500, height: 500,
-		gridSize: 40,
-		tree: {
-			"topper": "star",
-			"baubles": [],
-			"tinsels": [
-				[ [ 1, 1.5 ], [ 10, 2.5 ] ], [ [ 2, 4.5 ], [ 9, 5.5 ] ], [ [ 4, 7.5 ], [ 7, 8.5 ] ]
-			],
-			"outline": [
-				[ 1, 1.5 ], [ 4, 4.5 ], [ 2, 4.5 ], [ 5, 7.5 ], [ 4, 7.5 ], [ 6, 9.5 ], [ 8, 7.5 ], [ 7, 7.5 ], [ 10, 4.5 ], [ 8, 4.5 ], [ 11, 1.5 ]
-			]
-		},
-		cursor: {x: 1, y: 1},
-		plottingMode: "tree",
-		validation: {
-			valid: null,
-			status: "",
-			messages: []
-		},
-	}
-  },
-  methods: {
-    updateCursor: function(value) {
-		this.cursor.x = this.roundValue(value.x)
-		this.cursor.y = this.roundValue(value.y)
-	},
-	addPoint: function(value) {
-		if (this.plottingMode == "tree") {
-			this.tree.outline.push([
-				this.roundValue(value.x) / this.gridSize,
-				(this.height - this.roundValue(value.y)) / this.gridSize,
-			])
-		} else if (this.plottingMode == "bauble") {
-			this.tree.baubles.push([
-				this.roundValue(value.x) / this.gridSize,
-				(this.height - this.roundValue(value.y)) / this.gridSize,
-			])
-		} else if (this.plottingMode == "tinsel") {
-			var tinsels = this.tree.tinsels;
-			var point = [this.roundValue(value.x) / this.gridSize,
-						(this.height - this.roundValue(value.y)) / this.gridSize]
-			if (tinsels.length == 0) {
-				tinsels.push([point])
-			} else {
-				if (tinsels[tinsels.length-1].length < 2) {
-					tinsels[tinsels.length-1].push(point)
-				} else {
-					tinsels.push([point])
-				}
-			}
-			this.tree.tinsels = tinsels;
+	name: 'home',
+	data() {
+		return {
+			width: 500,
+			height: 500,
+			gridSize: 40,
+			tree: {
+				"topper": "star",
+				"baubles": [],
+				"tinsels": [
+					[
+						[1, 1.5],
+						[10, 2.5]
+					],
+					[
+						[2, 4.5],
+						[9, 5.5]
+					],
+					[
+						[4, 7.5],
+						[7, 8.5]
+					]
+				],
+				"outline": [
+					[1, 1.5],
+					[4, 4.5],
+					[2, 4.5],
+					[5, 7.5],
+					[4, 7.5],
+					[6, 9.5],
+					[8, 7.5],
+					[7, 7.5],
+					[10, 4.5],
+					[8, 4.5],
+					[11, 1.5]
+				]
+			},
+			cursor: {
+				x: 1,
+				y: 1
+			},
+			plottingMode: "tree",
+			validation: {
+				valid: null,
+				status: "",
+				messages: []
+			},
 		}
 	},
-	roundValue: function(value) {
-	  return Math.round(value / this.gridSize) * this.gridSize;
+	methods: {
+		updateCursor: function(value) {
+			this.cursor.x = this.roundValue(value.x)
+			this.cursor.y = this.roundValue(value.y)
+		},
+		addPoint: function(value) {
+			if (this.plottingMode == "tree") {
+				this.tree.outline.push([
+					this.roundValue(value.x) / this.gridSize,
+					(this.height - this.roundValue(value.y)) / this.gridSize,
+				])
+			} else if (this.plottingMode == "bauble") {
+				this.tree.baubles.push([
+					this.roundValue(value.x) / this.gridSize,
+					(this.height - this.roundValue(value.y)) / this.gridSize,
+				])
+			} else if (this.plottingMode == "tinsel") {
+				var tinsels = this.tree.tinsels;
+				var point = [this.roundValue(value.x) / this.gridSize,
+					(this.height - this.roundValue(value.y)) / this.gridSize
+				]
+				if (tinsels.length == 0) {
+					tinsels.push([point])
+				} else {
+					if (tinsels[tinsels.length - 1].length < 2) {
+						tinsels[tinsels.length - 1].push(point)
+					} else {
+						tinsels.push([point])
+					}
+				}
+				this.tree.tinsels = tinsels;
+			}
+		},
+		roundValue: function(value) {
+			return Math.round(value / this.gridSize) * this.gridSize;
+		},
+		renderResponse: function(data) {
+			this.validation.messages = data.messages;
+			this.validation.valid = data.valid;
+		},
+		validate: function(value) {
+			this.validation.status = "awaiting validation response";
+			var app = this;
+			axios.post("https://server.xmas-trees.charlieegan3.com/v0/data/tree/main/validate", this.tree)
+				.then(function(response) {
+					app.renderResponse(response.data)
+					app.validation.status = "";
+				})
+				.catch(function(error) {
+					app.validation.messages = ["Unable to validate"];
+					app.validation.status = "error";
+					console.log(error)
+				});
+		}
 	},
-	renderResponse: function(data) {
-		this.validation.messages = data.messages;
-		this.validation.valid = data.valid;
+	watch: {
+		tree: {
+			handler: function() {
+				this.validate()
+			},
+			deep: true
+		}
 	},
-	validate: function(value) {
-	  this.validation.status = "awaiting validation response";
-	  var app = this;
-      axios.post("https://server.xmas-trees.charlieegan3.com/v0/data/tree/main/validate", this.tree)
-        .then(function(response) {
-			app.renderResponse(response.data)
-	  app.validation.status = "";
-        })
-        .catch(function(error) {
-          app.validation.messages = ["Unable to validate"];
-	  app.validation.status = "error";
-          console.log(error)
-        });
+	components: {
+		Plotter
 	}
-  },
-  watch: {
-	  tree: {
-		  handler: function() { this.validate() },
-		  deep: true
-	  }
-  },
-  components: { Plotter }
 }
 </script>
